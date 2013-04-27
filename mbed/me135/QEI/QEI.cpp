69cdb36426d85738e3f66cf24807eb988bf3ec8a
Steven Rhodes <rhodes.steven@gmail.com>
2013-04-26 17:48:02 -0700

    Starting to put in speed control

--- mbed/me135/QEI/QEI.cpp
+++ mbed/me135/QEI/QEI.cpp
@@ -14,201 +14,54 @@
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @section DESCRIPTION
- *
- * Quadrature Encoder Interface.
- *
- * A quadrature encoder consists of two code tracks on a disc which are 90
- * degrees out of phase. It can be used to determine how far a wheel has
- * rotated, relative to a known starting position.
- *
- * Only one code track changes at a time leading to a more robust system than
- * a single track, because any jitter around any edge won't cause a state
- * change as the other track will remain constant.
- *
- * Encoders can be a homebrew affair, consisting of infrared emitters/receivers
- * and paper code tracks consisting of alternating black and white sections;
- * alternatively, complete disk and PCB emitter/receiver encoder systems can
- * be bought, but the interface, regardless of implementation is the same.
- *
- *               +-----+     +-----+     +-----+
- * Channel A     |  ^  |     |     |     |     |
- *            ---+  ^  +-----+     +-----+     +-----
- *               ^  ^
- *               ^  +-----+     +-----+     +-----+
- * Channel B     ^  |     |     |     |     |     |
- *            ------+     +-----+     +-----+     +-----
- *               ^  ^
- *               ^  ^
- *               90deg
- *
- * The interface uses X2 encoding by default which calculates the pulse count
- * based on reading the current state after each rising and falling edge of
- * channel A.
- *
- *               +-----+     +-----+     +-----+
- * Channel A     |     |     |     |     |     |
- *            ---+     +-----+     +-----+     +-----
- *               ^     ^     ^     ^     ^
- *               ^  +-----+  ^  +-----+  ^  +-----+
- * Channel B     ^  |  ^  |  ^  |  ^  |  ^  |     |
- *            ------+  ^  +-----+  ^  +-----+     +--
- *               ^     ^     ^     ^     ^
- *               ^     ^     ^     ^     ^
- * Pulse count 0 1     2     3     4     5  ...
- *
- * This interface can also use X4 encoding which calculates the pulse count
- * based on reading the current state after each rising and falling edge of
- * either channel.
- *
- *               +-----+     +-----+     +-----+
- * Channel A     |     |     |     |     |     |
- *            ---+     +-----+     +-----+     +-----
- *               ^     ^     ^     ^     ^
- *               ^  +-----+  ^  +-----+  ^  +-----+
- * Channel B     ^  |  ^  |  ^  |  ^  |  ^  |     |
- *            ------+  ^  +-----+  ^  +-----+     +--
- *               ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
- *               ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
- * Pulse count 0 1  2  3  4  5  6  7  8  9  ...
- *
- * It defaults
- *
- * An optional index channel can be used which determines when a full
- * revolution has occured.
- *
- * If a 4 pules per revolution encoder was used, with X4 encoding,
- * the following would be observed.
- *
- *               +-----+     +-----+     +-----+
- * Channel A     |     |     |     |     |     |
- *            ---+     +-----+     +-----+     +-----
- *               ^     ^     ^     ^     ^
- *               ^  +-----+  ^  +-----+  ^  +-----+
- * Channel B     ^  |  ^  |  ^  |  ^  |  ^  |     |
- *            ------+  ^  +-----+  ^  +-----+     +--
- *               ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
- *               ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
- *               ^  ^  ^  +--+  ^  ^  +--+  ^
- *               ^  ^  ^  |  |  ^  ^  |  |  ^
- * Index      ------------+  +--------+  +-----------
- *               ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
- * Pulse count 0 1  2  3  4  5  6  7  8  9  ...
- * Rev.  count 0          1           2
- *
- * Rotational position in degrees can be calculated by:
- *
- * (pulse count / X * N) * 360
- *
- * Where X is the encoding type [e.g. X4 encoding => X=4], and N is the number
- * of pulses per revolution.
- *
- * Linear position can be calculated by:
- *
- * (pulse count / X * N) * (1 / PPI)
- *
- * Where X is encoding type [e.g. X4 encoding => X=44], N is the number of
- * pulses per revolution, and PPI is pulses per inch, or the equivalent for
- * any other unit of displacement. PPI can be calculated by taking the
- * circumference of the wheel or encoder disk and dividing it by the number
- * of pulses per revolution.
  */
 
-/**
- * Includes
- */
 #include "QEI.h"
 
-QEI::QEI(PinName channelA,
-         PinName channelB,
-         PinName index,
-         int pulsesPerRev,
-         Encoding encoding) : channelA_(channelA), channelB_(channelB),
-        index_(index) {
+QEI::QEI(PinName channelA, PinName channelB) :
+    channelA_(channelA), channelB_(channelB) {
 
-    pulses_       = 0;
-    revolutions_  = 0;
-    pulsesPerRev_ = pulsesPerRev;
-    encoding_     = encoding;
+  pulses_ = 0;
 
-    //Workout what the current state is.
-    int chanA = channelA_.read();
-    int chanB = channelB_.read();
+  for (int i = 0; i < kNumOfOffsets; i++) {
+    offset_[i] = 0;
+  }
 
-    //2-bit state.
-    currState_ = (chanA << 1) | (chanB);
-    prevState_ = currState_;
+  //Workout what the current state is.
+  int chanA = channelA_.read();
+  int chanB = channelB_.read();
 
-    //X2 encoding uses interrupts on only channel A.
-    //X4 encoding uses interrupts on      channel A,
-    //and on channel B.
-    channelA_.rise(this, &QEI::encode);
-    channelA_.fall(this, &QEI::encode);
+  //2-bit state.
+  currState_ = (chanA << 1) | (chanB);
+  prevState_ = currState_;
 
-    //If we're using X4 encoding, then attach interrupts to channel B too.
-    if (encoding == X4_ENCODING) {
-        channelB_.rise(this, &QEI::encode);
-        channelB_.fall(this, &QEI::encode);
-    }
-    //Index is optional.
-    if (index !=  NC) {
-        index_.rise(this, &QEI::index);
-    }
+  timer_.start();
+
+  //X4 encoding uses interrupts on      channel A,
+  //and on channel B.
+  channelA_.rise(this, &QEI::encode_);
+  channelA_.fall(this, &QEI::encode_);
+  channelB_.rise(this, &QEI::encode_);
+  channelB_.fall(this, &QEI::encode_);
 
 }
 
-void QEI::reset(void) {
-
-    pulses_      = 0;
-    revolutions_ = 0;
-
+void QEI::reset(int ctr_num) {
+  offset_[ctr_num] = pulses_;
 }
 
 int QEI::getCurrentState(void) {
-
-    return currState_;
-
+  return currState_;
 }
 
-int QEI::getPulses(void) {
-
-    return pulses_;
-
+int QEI::getPulses(int ctr_num) {
+  return pulses_ - offset_ctr_num;
 }
 
-int QEI::getRevolutions(void) {
-
-    return revolutions_;
-
+int QEI::getPeriod(void) {
+  return period_;
 }
 
-// +-------------+
-// | X2 Encoding |
-// +-------------+
-//
-// When observing states two patterns will appear:
-//
-// Counter clockwise rotation:
-//
-// 10 -> 01 -> 10 -> 01 -> ...
-//
-// Clockwise rotation:
-//
-// 11 -> 00 -> 11 -> 00 -> ...
-//
-// We consider counter clockwise rotation to be "forward" and
-// counter clockwise to be "backward". Therefore pulse count will increase
-// during counter clockwise rotation and decrease during clockwise rotation.
-//
 // +-------------+
 // | X4 Encoding |
 // +-------------+
@@ -235,54 +88,26 @@ int QEI::getRevolutions(void) {
 // We might enter an invalid state for a number of reasons which are hard to
 // predict - if this is the case, it is generally safe to ignore it, update
 // the state and carry on, with the error correcting itself shortly after.
-void QEI::encode(void) {
+void QEI::encode_(void) {
 
-    int chanA  = channelA_.read();
-    int chanB  = channelB_.read();
+  int chanA  = channelA_.read();
+  int chanB  = channelB_.read();
 
-    //2-bit state.
-    currState_ = (chanA << 1) | (chanB);
+  //2-bit state.
+  currState_ = (chanA << 1) | (chanB);
 
-    if (encoding_ == X2_ENCODING) {
-
-        //11->00->11->00 is counter clockwise rotation or "forward".
-        if ((prevState_ == 0x3 && currState_ == 0x0) ||
-                (prevState_ == 0x0 && currState_ == 0x3)) {
-
-            pulses_++;
-
-        }
-        //10->01->10->01 is clockwise rotation or "backward".
-        else if ((prevState_ == 0x2 && currState_ == 0x1) ||
-                 (prevState_ == 0x1 && currState_ == 0x2)) {
-
-            pulses_--;
-
-        }
-
-    } else if (encoding_ == X4_ENCODING) {
-
-        //Entered a new valid state.
-        if (((currState_ ^ prevState_) != INVALID) && (currState_ != prevState_)) {
-            //2 bit state. Right hand bit of prev XOR left hand bit of current
-            //gives 0 if clockwise rotation and 1 if counter clockwise rotation.
-            int change = (prevState_ & PREV_MASK) ^ ((currState_ & CURR_MASK) >> 1);
-
-            if (change == 0) {
-                change = -1;
-            }
-
-            pulses_ -= change;
-        }
-
+  //Entered a new valid state.
+  if (((currState_ ^ prevState_) != INVALID) && (currState_ != prevState_)) {
+    //2 bit state. Right hand bit of prev XOR left hand bit of current
+    //gives 0 if clockwise rotation and 1 if counter clockwise rotation.
+    int change = (prevState_ & PREV_MASK) ^ ((currState_ & CURR_MASK) >> 1);
+    if (change == 0) {
+      change = -1;
     }
-
-    prevState_ = currState_;
-
-}
-
-void QEI::index(void) {
-
-    revolutions_++;
-
+    pulses_ -= change;
+    int time = timer.read_us();
+    period_ = (time - last_time_) / 2 + period_ / 2;
+    last_time_ = time;
+  }
+  prevState_ = currState_;
 }
