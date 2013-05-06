7ea07b43d98bfa2a61fc620e7cb7f7ed156e2c68
Steven Rhodes <rhodes.steven@gmail.com>
2013-05-05 21:48:23 -0700

    Changing claw behavior, tweaking straightening code

--- mbed/me135/main.cpp
+++ mbed/me135/main.cpp
@@ -10,10 +10,12 @@ QEI right_enc(p13, p14);
 me135::IRSensor front_ir(p15);
 me135::IRSensor left_ir(p16);
 me135::IRSensor right_ir(p17);
+me135::IRSensor right_ir_secondary(p18);
 me135::DriveTrain right_drive(p21, p22);
 me135::DriveTrain left_drive(p24, p23);
 me135::Shooter shooter(p28, p26, p25);
 me135::Claw claw(p29);
+me135::Sonar sonar(p27, p30);
 
 volatile int g_left_target_speed = 0;
 volatile int g_right_target_speed = 0;
@@ -66,16 +68,17 @@ void send_walls(Directions dir) {
   bone.write(buffer, len);
 }
 
-void send_claw_pos(const float claw_pos) {
+void send_claw_pos(const float claw_pos, const bool grabbed) {
   int pos = (int)(claw_pos * 99);
   char buffer[128];
   char len = sprintf(buffer, "{" KEY("id","claw")
-                             "," KEY("pos",%d) "}\n", pos);
+                             "," KEY("pos",%d)
+                             "," KEY("grabbed",%d) "}\n",
+                             pos, grabbed);
   bone.write(buffer, len);
 }
 
 bool dist_control(const int final, const Directions dir) {
-//  static int i_term = 0;
   int dist;
   if (dir == FWD) {
     dist = min(left_enc.getPulses(0), right_enc.getPulses(0));
@@ -86,29 +89,56 @@ bool dist_control(const int final, const Directions dir) {
   } else {
     dist = 0;
   }
-  // TODO: Use walls to go straight
+
   int error = final - dist;
   int target = kDistPNumerator * error / kDistPDenominator + kDistOL;
 
-  // Ugly, should be refactored
-  int straighten = 0;
-  if (dir == FWD) {
-    float left_ir_val = left_ir;
-    float right_ir_val = right_ir;
-    if (left_ir_val < kWallDist && right_ir_val < kWallDist) {
-      straighten = (int) (kStraighten * (left_ir_val - right_ir_val));
-    }
+  // Early return if going forwards and we hit a wall
+  if (front_ir < kFrontWallDist) {
+    g_left_target_speed = 0;
+    g_right_target_speed = 0;
+    return true;
   }
+
   if (dist < final) {
     if (dir == LEFT) {
       g_left_target_speed = -target;
     } else {
-      g_left_target_speed = target - straighten;
+      g_left_target_speed = target;
     }
     if (dir == RIGHT) {
       g_right_target_speed = -target;
     } else {
-      g_right_target_speed = target + straighten;
+      g_right_target_speed = target;
+    }
+
+    // Ugly, should be refactored
+    if (dir == FWD) {
+      float left_ir_val = left_ir;
+      float right_ir_val = right_ir;
+      // Check if we have walls on both sides
+      if (left_ir_val < kWallDist && right_ir_val < kWallDist) {
+        float ir_error = left_ir_val - right_ir_val;
+        if (abs(ir_error) < kCloseEnoughToMiddle) {
+          // Straighten robot, if possible
+          // Do this by shutting off one side
+          float right_ir_secondary_val = right_ir_secondary;
+          if (right_ir_secondary_val < kWallDist) {
+            float same_side_ir_error = right_ir_val - right_ir_secondary_val;
+            if (same_side_ir_error > kStraightenSameSide) {
+              // back right ir is farther, robot is tilted to the right, need to move right side to compensate
+              g_left_target_speed = 0;
+            } else if (same_side_ir_error < kStraightenSameSide) {
+              g_right_target_speed = 0;
+            }
+          }
+        } else {
+          // Move towards center
+          int straighten = (int) (kStraighten * ir_error);
+          g_left_target_speed -= straighten;
+          g_right_target_speed += straighten;
+        }
+      }
     }
     return false;
   } else {
@@ -116,6 +146,7 @@ bool dist_control(const int final, const Directions dir) {
     g_right_target_speed = 0;
     return true;
   }
+
 }
 
 // PI Control
@@ -138,12 +169,14 @@ void speed_control(void) {
     right_drive = 0;
   }
 
+  // Do left side
   int l_error = g_left_target_speed - left_speed;
   i_term_l += kSpeedI * l_error;
   i_term_l = constrain(i_term_l, kMinITerm, kMaxITerm);
   int left_power = kSpeedOL * g_left_target_speed + kSpeedP * l_error + i_term_l;
   left_drive = constrain(left_power / kMaxPrescaledSpeed, -1.0f, 1.0f);
 
+  // Do right side
   int r_error = g_right_target_speed - right_speed;
   i_term_r += kSpeedI * r_error;
   i_term_r = constrain(i_term_r, kMinITerm, kMaxITerm);
@@ -183,7 +216,21 @@ void coast(void) {
   left_drive = 0;
 }
 
+// Will close claw if the sensor sees it
+void check_claw(void) {
+  if (sonar < kGrabbingDistance) {
+     claw = 1;
+     send_claw_pos(claw.read(), true);
+  }
+}
+
 int main() {
+  Timer claw_timer;
+  claw_timer.start();
+
+  Timer time_since_grabbed;
+  time_since_grabbed.start();
+
   Ticker speed_loop_ticker;
   speed_loop_ticker.attach_us(speed_control, kSpeedControlLoopTime);
 
@@ -199,8 +246,15 @@ int main() {
   Directions target_dir = STOP; // Used in distance mode;
   Modes mode = IDLE;
 
+
+
   char msg[kMaxMsgSize];
   for (;;) {
+    if (claw_timer > kCheckClawTime &&
+        time_since_grabbed > kDelayClawTime) {
+      claw_timer.reset();
+      check_claw();
+    }
     switch (mode) {
       case MOVING:
         if (dist_control_loop_timer.read_us() > kDistControlLoopTime) {
@@ -282,7 +336,8 @@ int main() {
         case 'c': {
           int pos = (msg[1] - '0') * 10 + (msg[2] - '0');
           claw = pos / kMaxClawPos;
-          send_claw_pos(claw.read());
+          time_since_grabbed.reset();
+          send_claw_pos(claw.read(), false);
           break;
         }
         // Shooter
--- mbed/me135/me135.h
+++ mbed/me135/me135.h
@@ -13,11 +13,18 @@
 #include "BeagleBone.h"
 #include "Shooter.h"
 #include "IRSensor.h"
+#include "Sonar.h"
 #include "QEI.h"
 
+const float kFrontWallDist = 2.8;
 const float kWallDist = 8;
+const float kGrabbingDistance = 3;
+const float kCloseEnoughToMiddle = 0.5;
+const float kStraightenSameSide = 0.1; // Minimum difference that we still straighten
 const int kDistControlLoopTime = 50000; // in us
 const int kSpeedControlLoopTime = 50000; // in us
+const float kCheckClawTime = 0.2;
+const float kDelayClawTime = 2;
 const int kSendDataTime = 200000; // in us, time between sending graph data
 const int kQuarterCircle = 390;
 const int kUsPerS = 1000000;
@@ -25,13 +32,13 @@ const int kSpeedScaling = 1;
 const int kSpeedI = 10;
 const int kSpeedOL = 60;
 const int kSpeedP = 60;
-const int kStraighten = 5;
+const float kStraighten = 5;
 const int kDistPNumerator = 0;//1000;
 const int kDistPDenominator = 10000;
 const int kDistOL = 20;
 const int kMaxITerm = 1000;
 const int kMinITerm = -1000;
-const int kClicksPerInch = 40;
+const int kClicksPerInch = 42;
 const float kMaxPrescaledSpeed = 3000;
 const float kSquareSize = 14; // in inches
 const int kMaxMsgSize = 10;
@@ -54,6 +61,7 @@ typedef enum {
   MOVING
 } Modes;
 
+
 template <typename T>
 inline T constrain(T x, T min, T max){
   return x > max ? max : (x < min ? min : x);
@@ -72,6 +80,11 @@ inline T max(T x, T y) {
 template <typename T>
 inline T min(T x, T y) {
   return x < y ? x : y;
+}
+
+template <typename T>
+inline T abs(T x) {
+  return x > 0 ? x : -x;
 }
 
 inline Directions charToDirection(char c) {
